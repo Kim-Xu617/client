3f99a1f1b3dce39e913f03911c332b3d7ba60624
xwu <xuw@tcd.ie>
2024-04-14 16:16:44 +0100

    Import React Rount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <section>Home</section>,
+  },
+  {
+    path: "/products/:id",
+    element: <section>Catergories</section>,
+  },
+  {
+    path: "/product/:id",
+    element: <section>Product</section>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
